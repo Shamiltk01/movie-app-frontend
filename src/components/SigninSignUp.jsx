3a3377abd1dc7b1d70f252c6d6876b18a8c83619
Shamiltk01 <shamiltk02@gmail.com>
2024-03-04 11:54:19 +0530

    full login changed

--- src/components/SigninSignUp.jsx
+++ src/components/SigninSignUp.jsx
@@ -1,47 +1,117 @@
 import React, { useState } from 'react';
 
-const SignInSignUp = () => {
-  const [isSignIn, setIsSignIn] = useState(true);
+
+const SigninSignUp = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
 
   const toggleForm = () => {
-    setIsSignIn((prev) => !prev);
+    setIsSignUp((prev) => !prev);
   };
 
   return (
-    <div className={`container ${isSignIn ? '' : 'active'}`}>
-      <div className={`form-container sign-up`}>
-        <form>
-          <h1>Create Account</h1>
-          <br />
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button>Sign Up</button>
-        </form>
-      </div>
-      <div className={`form-container sign-in`}>
-        <form>
-          <h1>Sign In</h1>
-          <br />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <a href="#">Forget Your Password?</a>
-          <button>Sign In</button>
-        </form>
-      </div>
-      <div className="toggle-container">
-        <div className="toggle">
-          <div className="toggle-panel toggle-left">
-            {/* ... your sign-in toggle content ... */}
-            <button className="hidden" onClick={toggleForm}>
-              Sign In
-            </button>
-          </div>
-          <div className="toggle-panel toggle-right">
-            {/* ... your sign-up toggle content ... */}
-            <button className="hidden" onClick={toggleForm}>
-              Sign Up
-            </button>
+    <div className={`section full-height ${isSignUp ? 'signup-mode' : ''}`}>
+      <a href="#" className="logo" target="_blank">
+        <img src="https://assets.codepen.io/1462889/fcy.png" alt="" />
+      </a>
+
+      <div className="container">
+        <div className="row full-height justify-content-center">
+          <div className="col-12 text-center align-self-center py-5">
+            <div className="section pb-5 pt-5 pt-sm-2 text-center">
+              <h6 className="mb-0 pb-3">
+                <span onClick={() => setIsSignUp(false)}>Log In </span>
+                <span onClick={() => setIsSignUp(true)}>Sign Up</span>
+              </h6>
+              <input
+                className="checkbox"
+                type="checkbox"
+                id="reg-log"
+                name="reg-log"
+                checked={isSignUp}
+                onChange={toggleForm}
+              />
+              <label htmlFor="reg-log"></label>
+              <div className="card-3d-wrap mx-auto">
+                <div className={`card-3d-wrapper ${isSignUp ? 'is-signup' : ''}`}>
+                  <div className="card-front">
+                    <div className="center-wrap">
+                      <div className="section text-center">
+                        <h4 className="mb-4 pb-3">Log In</h4>
+                        <div className="form-group">
+                          <input
+                            type="email"
+                            name="logemail"
+                            className="form-style"
+                            placeholder="Your Email"
+                            id="logemail"
+                            autoComplete="off"
+                          />
+                          <i className="input-icon uil uil-at"></i>
+                        </div>
+                        <div className="form-group mt-2">
+                          <input
+                            type="password"
+                            name="logpass"
+                            className="form-style"
+                            placeholder="Your Password"
+                            id="logpass"
+                            autoComplete="off"
+                          />
+                          <i className="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <button className="btn mt-4">Submit</button>
+                        <p className="mb-0 mt-4 text-center">
+                          <a href="#0" className="link">
+                            Forgot your password?
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card-back">
+                    <div className="center-wrap">
+                      <div className="section text-center">
+                        <h4 className="mb-4 pb-3">Sign Up</h4>
+                        <div className="form-group">
+                          <input
+                            type="text"
+                            name="logname"
+                            className="form-style"
+                            placeholder="Your Full Name"
+                            id="logname"
+                            autoComplete="off"
+                          />
+                          <i className="input-icon uil uil-user"></i>
+                        </div>
+                        <div className="form-group mt-2">
+                          <input
+                            type="email"
+                            name="logemail"
+                            className="form-style"
+                            placeholder="Your Email"
+                            id="logemail"
+                            autoComplete="off"
+                          />
+                          <i className="input-icon uil uil-at"></i>
+                        </div>
+                        <div className="form-group mt-2">
+                          <input
+                            type="password"
+                            name="logpass"
+                            className="form-style"
+                            placeholder="Your Password"
+                            id="logpass"
+                            autoComplete="off"
+                          />
+                          <i className="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <button className="btn mt-4">Submit</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -49,4 +119,4 @@ const SignInSignUp = () => {
   );
 };
 
-export default SignInSignUp;
+export default SigninSignUp;
