3bc24c5b5b0cfac17133e84949189c820dd01c17
avanthik33 <avanthik33@gmail.com>
2024-03-07 00:47:36 +0530

    completed signin and signup

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
+import React from "react";
 
-import React from 'react';
+import "./App.css";
 
+import SignInSignUp from "./components/SigninSignUp";
 
-import './App.css';
+import AdimAdd from "./components/AdimAdd";
+import DeleteMovie from "./components/DeleteMovie";
+import ViewBookings from "./components/ViewBookings";
 
-import SignInSignUp from './components/SigninSignUp';
+import Card2 from "./components/Card2";
+import { BrowserRouter } from "react-router-dom";
+import {
+  Route,
+  Routes,
+} from "react-router-dom/dist/umd/react-router-dom.development";
 
-import AdimAdd from './components/AdimAdd';
-import DeleteMovie from './components/DeleteMovie';
-import ViewBookings from './components/ViewBookings';
-
-
-import Card2 from './components/Card2';
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom/dist/umd/react-router-dom.development';
-
-import Intro from './components/Intro';
-
+import Intro from "./components/Intro";
+import AcceptUser from "./components/AcceptUser";
+import SigninSignUp from "./components/SigninSignUp";
 
 const App = () => {
   return (
-<div>
-
+    <div>
       <BrowserRouter>
         <Routes>
+          {/* home page */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Intro />
+                <Card2 />
+              </>
+            }
+          />
 
-            <Route path="/" element={<><Intro/><Card2/></>}/>
-            <Route path="/moviebooking" element={<AdimAdd/>}/>
-            <Route path="/deletemovie" element={<DeleteMovie/>}/>
-            <Route path="/viewbooking" element={<ViewBookings/>}/>
+            {/* other routes */}
+            <Route path="/signin" element={<SigninSignUp />} />
+          <Route path="/moviebooking" element={<AdimAdd />} />
+          <Route path="/deletemovie" element={<DeleteMovie />} />
+          <Route path="/viewbooking" element={<ViewBookings />} />
+          <Route path="/acceptuser" element={<AcceptUser />} />
 
         </Routes>
       </BrowserRouter>
-
-</div>
+    </div>
   );
 };
 
