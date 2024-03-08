0c426db11380bc926347d81b7469273bfc662bf9
Shamiltk01 <shamiltk02@gmail.com>
2024-03-09 00:00:42 +0530

    edit

--- src/components/SigninSignUp.jsx
+++ src/components/SigninSignUp.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import homelogo from '../images/home.png'
 
 const SigninSignUp = () => {
   const navigate = useNavigate();
@@ -52,7 +53,7 @@ const SigninSignUp = () => {
     <div className={`section full-height ${isSignUp ? "signup-mode" : ""}`}>
       <Link to="/" className="logo">
         <img
-          src="https://static.vecteezy.com/system/resources/previews/011/934/381/original/gold-home-icon-free-png.png"
+          src={homelogo}
           alt=""
         />
       </Link>
